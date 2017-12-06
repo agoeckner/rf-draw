84d864c5a10a47574ef1157c954d166e99d2bc79
Krutarth Rao <krutarth.rao@hpe.com>
2017-12-06 00:46:36 -0500

    parent logging script added

---
+++ rf-draw/appRunner.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+
+APP_PATH="/usr/__main__.py"
+APP="python3 $APP_PATH"
+log_file=app_runner.log
+
+curr_time=$(date "+%Y.%m.%d-%H.%M.%S")
+echo "[$curr_time] rf-draw Started.." >> $log_file
+
+until $APP; do
+	curr_time=$(date "+%Y.%m.%d-%H.%M.%S")
+    echo "[$curr_time] rf-draw crashed with exit code $?.  Respawning.." >> $log_file
+    # ping the switch?
+    sleep 1
+done
+
+curr_time=$(date "+%Y.%m.%d-%H.%M.%S")
+echo "**********[$curr_time] rf-draw crashed with exit code 0**********" >> $log_file
